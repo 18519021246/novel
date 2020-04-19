375e0b5db11c3df0b070754c36c9679cff4f9bba
gaotong12 <gaotong12@jd.com>
2020-04-19 21:27:06 +0800

    代码提交

---
+++ src/main/java/com/imnu/service/impl/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.imnu.service.impl;
+
+import com.imnu.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service("/userService")
+public class UserServiceImpl implements UserService{
+}
